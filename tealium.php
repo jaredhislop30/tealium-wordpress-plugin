724081c9e10a1b1a6a93f93adc2f452a81eeb397
Jared Hislop <jared.hislop@challengervault.com>
2018-11-30 23:30:03 -0700

    explode categories

--- tealium.php
+++ tealium.php
@@ -283,8 +283,7 @@ function tealiumWooCommerceData( $utagdata ) {
 	    $productData['product_list_price'][] = $product->get_regular_price();
 	    $productData['product_sale_price'][] = $product->get_sale_price();
 	    $productData['product_image_url'][] = get_the_post_thumbnail_url( $product->get_id(), 'full' );
-	    $categories = wc_get_product_category_list($product->get_id());
-	    print($categories);
+	    $categories = explode(",", wc_get_product_category_list($product->get_id()));
 	    $productData['product_cateogry'][] = $categories[0];
 	    $productData['product_subcateogry'][] = $categories[1];
 	    $productData['product_subcateogry2'][] = $categories[2];
