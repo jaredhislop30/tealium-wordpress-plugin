5d0431b8e298207a936a0f9d974d3752be5decda
Jared Hislop <jared.hislop30@gmail.com>
2018-12-28 13:10:27 -0700

    update for product quantity

--- js/tealium-woocommerce-tracking.js
+++ js/tealium-woocommerce-tracking.js
@@ -34,6 +34,7 @@ jQuery( document ).on( 'click', '.single_add_to_cart_button', function() {
             var teal_data = {}
             teal_data.tealium_event = "cart_add";
             teal_data.currency_code = _product_currency;
+            teal_data.product_quantity = [jQuery( 'form.cart:first input[name=quantity]' ).val()];
             var prod_data = Object.assign({}, teal_data, teal_current_prod_variation);
             utag.link(prod_data);
         }
