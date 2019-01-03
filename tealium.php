7ebe29c50595962616fe152ea93c12770ce535ac
Jared Hislop <jared.hislop30@gmail.com>
2019-01-03 15:29:46 -0700

    bump version

--- tealium.php
+++ tealium.php
@@ -397,7 +397,7 @@ function tealiumDataObject() {
 	$utagdata = array();
 
 	// Set Default Data. May be overwritten below / later
-	$utagdata['plugin_version'] = "0.0.5";
+	$utagdata['plugin_version'] = "0.0.6";
 	$utagdata['siteName'] = get_bloginfo( 'name' );
 	$utagdata['siteDescription'] = get_bloginfo( 'description' );
 	$utagdata['language_code'] = explode("_",get_locale())[0];
